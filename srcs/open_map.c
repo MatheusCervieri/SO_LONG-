81388525bc24d8b729b9210cc771c46faa308796
MatheusCervieri <mathcervieri@gmail.com>
2022-07-14 23:12:44 +0200

    Finished validation_first_and_last_line function

--- srcs/open_map.c
+++ srcs/open_map.c
@@ -1,5 +1,29 @@
 #include "so_long.h"
 
+void validation_first_and_last_line(char *line, t_game *game)
+{
+	unsigned int i;
+	i = 0;
+	while(line[i] != '\0')
+	{
+		if(line[i] != '1' && line[i] != '\n')
+			error_close("The map must be surrounded by walls", game);
+		i++;
+	}
+}
+/*
+void validation_mid_lines(char *line, t_game *game)
+{
+	unsigned int i;
+	i = 0;
+	while(line[i] != '\0')
+	{
+		if(line[i] != '1' && line[i] != '\n')
+			error_close("The map must be surrounded by walls", game);
+		i++;
+	}
+}
+*/
 
 void open_map_validation(t_game *game)
 {
@@ -13,10 +37,11 @@ void open_map_validation(t_game *game)
 	if(fd < 0)
 		error_close("Cannot Read File!", game);
 	map_line = get_next_line(fd);
+	if(map_line == NULL)
+		error_close("Empty file", game); 
 	ft_printf("%s", map_line);
 	width = ft_strlen(map_line);
-	if(map_line == NULL)
-		error_close("Empty file", game); //PENSAR MELHOR AQUI. 
+	validation_first_and_last_line(map_line, game); 
 	height++;
 	while(map_line)
 	{
@@ -26,18 +51,21 @@ void open_map_validation(t_game *game)
 		if(is_there_nl(map_line) != -1)
 		{
 			if(ft_strlen(map_line) != width)
-				error_close("It is not rectangular!", game);
+				error_close("The map is not rectangular!", game);
 		}
-		if(is_there_nl(map_line) == -1)
+		if(is_there_nl(map_line) == -1) //é a última linha.
 		{
+			validation_first_and_last_line(map_line, game); 
 			if(ft_strlen(map_line) != width - 1)
-				error_close("It is not rectangular!", game);
+				error_close("The map is not rectangular!", game);
 		}
-	
 			ft_printf("%s", map_line);
-		height++;
+			height++;
 		}
 		free(map_line);
 	}
 	close(fd);
 }
+
+//map with walls
+//map composition  - 0,1,C,E,P
